30bb003cbd2ad7c90fb70616b580d31677894c26
Kizito Nwose <kizitonwose@gmail.com>
2018-02-16 17:42:22 +0100

    Use default protocol implementations where necessary.

--- CountryPickerView/CountryPickerView.swift
+++ CountryPickerView/CountryPickerView.swift
@@ -8,69 +8,9 @@
 
 import UIKit
 
-public protocol CountryPickerViewDelegate: class {
-    /// Called when the user selects a country from the list.
-    func countryPickerView(_ countryPickerView: CountryPickerView, didSelectCountry country: Country)
-}
 
-public protocol CountryPickerViewDataSource: class {
-    /// An array of countries you wish to show at the top of the list. 
-    /// This is useful if your app is targeted towards people in specific countries.
-    /// - requires: The title for the section to be returned in `sectionTitleForPreferredCountries`
-    func preferredCountries(in countryPickerView: CountryPickerView) -> [Country]
-    
-    /// The desired title for the preferred section.
-    /// - **See:** `preferredCountries` method. Both are required for the section to be shown.
-    func sectionTitleForPreferredCountries(in countryPickerView: CountryPickerView) -> String?
-    
-    /// This determines if only the preferred section is shown
-    func showOnlyPreferredSection(in countryPickerView: CountryPickerView) -> Bool
-    
-    /// The navigation item title when the internal view controller is pushed/presented.
-    func navigationTitle(in countryPickerView: CountryPickerView) -> String?
-    
-    /// A navigation item button to be used if the internal view controller is presented(not pushed). 
-    /// Return `nil` to use a default "Close" button.
-    func closeButtonNavigationItem(in countryPickerView: CountryPickerView) -> UIBarButtonItem?
-    
-    /// The desired position for the search bar.
-    func searchBarPosition(in countryPickerView: CountryPickerView) -> SearchBarPosition
-    
-    /// This determines if a country's phone code is shown alongside the country's name on the list.
-    /// e.g Nigeria (+234)
-    func showPhoneCodeInList(in countryPickerView: CountryPickerView) -> Bool
-}
-
-// MARK:- CountryPickerViewDataSource default implementations
-public extension CountryPickerViewDataSource {
-    
-    func preferredCountries(in countryPickerView: CountryPickerView) -> [Country] {
-        return []
-    }
-    
-    func sectionTitleForPreferredCountries(in countryPickerView: CountryPickerView) -> String? {
-        return nil
-    }
-    
-    func showOnlyPreferredSection(in countryPickerView: CountryPickerView) -> Bool {
-        return false
-    }
-    
-    func navigationTitle(in countryPickerView: CountryPickerView) -> String? {
-        return nil
-    }
-    
-    func closeButtonNavigationItem(in countryPickerView: CountryPickerView) -> UIBarButtonItem? {
-        return nil
-    }
-    
-    func searchBarPosition(in countryPickerView: CountryPickerView) -> SearchBarPosition {
-        return .tableViewHeader
-    }
-    
-    func showPhoneCodeInList(in countryPickerView: CountryPickerView) -> Bool {
-        return false
-    }
+public enum SearchBarPosition {
+    case tableViewHeader, navigationBar, hidden
 }
 
 public struct Country {
@@ -94,10 +34,6 @@ public func ==(lhs: Country, rhs: Country) -> Bool {
 }
 public func !=(lhs: Country, rhs: Country) -> Bool {
     return lhs.code != rhs.code
-}
-
-public enum SearchBarPosition {
-   case tableViewHeader, navigationBar, hidden
 }
 
 
@@ -266,10 +202,10 @@ extension CountryPickerView {
 }
 
 // MARK:- An internal implementation of the CountryPickerViewDataSource.
-// Returns default options where necessary.
-extension CountryPickerView {
+// Returns default options where necessary if the data source is not set.
+extension CountryPickerView: CountryPickerViewDataSource {
     var preferredCountries: [Country] {
-        return dataSource?.preferredCountries(in: self) ?? [Country]()
+        return dataSource?.preferredCountries(in: self) ?? preferredCountries(in: self)
     }
     
     var preferredCountriesSectionTitle: String? {
@@ -277,7 +213,7 @@ extension CountryPickerView {
     }
     
     var showOnlyPreferredSection: Bool {
-        return dataSource?.showOnlyPreferredSection(in: self) ?? false
+        return dataSource?.showOnlyPreferredSection(in: self) ?? showOnlyPreferredSection(in: self)
     }
     
     var navigationTitle: String? {
@@ -292,11 +228,11 @@ extension CountryPickerView {
     }
     
     var searchBarPosition: SearchBarPosition {
-        return dataSource?.searchBarPosition(in: self) ?? .tableViewHeader
+        return dataSource?.searchBarPosition(in: self) ?? searchBarPosition(in: self)
     }
     
     var showPhoneCodeInList: Bool {
-        return dataSource?.showPhoneCodeInList(in: self) ?? false
+        return dataSource?.showPhoneCodeInList(in: self) ?? showPhoneCodeInList(in: self)
     }
     
 }
