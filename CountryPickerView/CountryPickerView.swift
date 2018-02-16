c6d58dc018f7881c18073ddfc2e3ddbfe11bc052
Kizito Nwose <kizitonwose@gmail.com>
2018-02-16 17:15:08 +0100

    Add default protocol implementations for CountryPickerViewDataSource

--- CountryPickerView/CountryPickerView.swift
+++ CountryPickerView/CountryPickerView.swift
@@ -8,23 +8,23 @@
 
 import UIKit
 
-public protocol CountryPickerViewDelegate: NSObjectProtocol {
+public protocol CountryPickerViewDelegate: class {
     /// Called when the user selects a country from the list.
     func countryPickerView(_ countryPickerView: CountryPickerView, didSelectCountry country: Country)
 }
 
-public protocol CountryPickerViewDataSource: NSObjectProtocol {
+public protocol CountryPickerViewDataSource: class {
     /// An array of countries you wish to show at the top of the list. 
     /// This is useful if your app is targeted towards people in specific countries.
     /// - requires: The title for the section to be returned in `sectionTitleForPreferredCountries`
-    func preferredCountries(in countryPickerView: CountryPickerView) -> [Country]?
+    func preferredCountries(in countryPickerView: CountryPickerView) -> [Country]
     
     /// The desired title for the preferred section.
     /// - **See:** `preferredCountries` method. Both are required for the section to be shown.
     func sectionTitleForPreferredCountries(in countryPickerView: CountryPickerView) -> String?
     
     /// This determines if only the preferred section is shown
-    func showOnlyPreferredSection(in countryPickerView: CountryPickerView) -> Bool?
+    func showOnlyPreferredSection(in countryPickerView: CountryPickerView) -> Bool
     
     /// The navigation item title when the internal view controller is pushed/presented.
     func navigationTitle(in countryPickerView: CountryPickerView) -> String?
@@ -38,7 +38,39 @@ public protocol CountryPickerViewDataSource: NSObjectProtocol {
     
     /// This determines if a country's phone code is shown alongside the country's name on the list.
     /// e.g Nigeria (+234)
-    func showPhoneCodeInList(in countryPickerView: CountryPickerView) -> Bool?
+    func showPhoneCodeInList(in countryPickerView: CountryPickerView) -> Bool
+}
+
+// MARK:- CountryPickerViewDataSource default implementations
+public extension CountryPickerViewDataSource {
+    
+    func preferredCountries(in countryPickerView: CountryPickerView) -> [Country] {
+        return []
+    }
+    
+    func sectionTitleForPreferredCountries(in countryPickerView: CountryPickerView) -> String? {
+        return nil
+    }
+    
+    func showOnlyPreferredSection(in countryPickerView: CountryPickerView) -> Bool {
+        return false
+    }
+    
+    func navigationTitle(in countryPickerView: CountryPickerView) -> String? {
+        return nil
+    }
+    
+    func closeButtonNavigationItem(in countryPickerView: CountryPickerView) -> UIBarButtonItem? {
+        return nil
+    }
+    
+    func searchBarPosition(in countryPickerView: CountryPickerView) -> SearchBarPosition {
+        return .tableViewHeader
+    }
+    
+    func showPhoneCodeInList(in countryPickerView: CountryPickerView) -> Bool {
+        return false
+    }
 }
 
 public struct Country {
